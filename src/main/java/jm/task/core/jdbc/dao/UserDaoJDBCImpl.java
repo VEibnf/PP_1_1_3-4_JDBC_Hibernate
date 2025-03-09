c2de0ab9cd9cd7cf15806ee3825eab3c376dc997
VEibnf <Ve.infobiz@gmail.com>
2025-03-09 21:01:18 +0700

    1/1/3 ready

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -9,27 +12,87 @@ public class UserDaoJDBCImpl implements UserDao {
 
     }
 
-    public void createUsersTable() {
+    private Util util = new Util();
 
+    public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS users (" +
+                "id BIGSERIAL PRIMARY KEY, " +
+                "name VARCHAR(50), " +
+                "lastName VARCHAR(50), " +
+                "age SMALLINT)";
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void dropUsersTable() {
 
+    public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users";
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+        try (Connection connection = util.getConnection();
+             PreparedStatement pstnt = connection.prepareStatement(sql)) {
+            pstnt.setString(1, name);
+            pstnt.setString(2, lastName);
+            pstnt.setByte(3, age);
+            pstnt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
+        String sql = "DELETE FROM users WHERE id = ?";
+        try (Connection connection = util.getConnection();
+             PreparedStatement pstnt = connection.prepareStatement(sql)) {
+            pstnt.setLong(1, id);
+            pstnt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String sql = "SELECT * FROM users";
+        List<User> users = new ArrayList<>();
+        try (Connection connection = util.getConnection();
+             Statement stmt = connection.createStatement();
+             ResultSet resultSet = stmt.executeQuery(sql)) {
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        String sql = "DELETE FROM users";
+        try (Connection connection = util.getConnection();
+             PreparedStatement pstnt = connection.prepareStatement(sql)) {
+            pstnt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "18091966";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
+
